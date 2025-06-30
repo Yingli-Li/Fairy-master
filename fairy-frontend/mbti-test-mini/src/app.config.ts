fc739791090517cfb04115499de9e2ae6f428dc7
Yingli-Li <201026563@qq.com>
2025-06-30 21:39:31 +0800

    Delete fairy-frontend/mbti-test-mini directory

--- fairy-frontend/mbti-test-mini/src/app.config.ts
+++
@@ -1,9 +0,0 @@
-export default defineAppConfig({
-  pages: ["pages/index/index", "pages/result/index", "pages/doQuestion/index"],
-  window: {
-    backgroundTextStyle: "light",
-    navigationBarBackgroundColor: "#fff",
-    navigationBarTitleText: "鱼皮 MBTI 性格测试",
-    navigationBarTextStyle: "black",
-  },
-});
